cc3b55c99cd49dc699a187f6b4ac16bef8ef8a41
jero142 <180901023+jero142@users.noreply.github.com>
2024-12-09 23:44:14 -0500

    Create each possibility for a  round

--- main.js
+++ main.js
@@ -5,3 +5,29 @@ const computerPlay = () => {
     return getComputerChoice
 }
 
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection ) {
+        return `You tied! You both picked ${playerSelection}`
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        compScore++
+        return 'You lost! Rock crushes Scissors'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++
+        return 'You won! Scissors cuts Paper'
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        compScore++
+        return 'You lost! Paper covers Rock'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore++
+        return 'You won! Rock crushes Scissors'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        compScore++
+        return 'You lost! Scissors cuts Paper'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore++
+        return 'You won! Paper covers Rock'
+    }   
+
+}
+
+const playerSelection = 'rock'
